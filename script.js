027d920a55a29135ffa78c4e04a2082afd685cde
Ahmed Ogundimu <ogundimu.ahmed@gmail.com>
2020-04-20 01:18:19 +0100

    Added the testimonial section and dropdown part of the navbar

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 let slides = document.querySelectorAll(".slide");
 let currentSlide = 0;
+let previous = document.querySelector(".left-slider-nav");
+let next = document.querySelector(".right-slider-nav");
 
 function nextSlide() {
     goToSlide(currentSlide + 1);
@@ -16,13 +18,31 @@ function goToSlide(n) {
     slides[currentSlide].className = 'slide showing';
 }
 
-let previous = document.querySelector(".left-slider-nav");
-let next = document.querySelector(".right-slider-nav");
+next.addEventListener("click", nextSlide);
+previous.addEventListener("click", previousSlide);
 
-next.onclick = function() {
-    nextSlide();
-};
+/* Testimonials Slider */
 
-previous.onclick = function() {
-    previousSlide();
-};
+
+let testimonialSliders = document.querySelectorAll(".mySlides");
+let showingSlide = 0;
+let prevTest = document.querySelector(".prev");
+let nextTest = document.querySelector(".next");
+
+
+function nextTestimonial() {
+    goToTestimonial(showingSlide+1);
+}
+
+function prevTestimonial() {
+    goToTestimonial(showingSlide-1);
+}
+
+function goToTestimonial(n) {
+    testimonialSliders[showingSlide].className = "mySlides";
+    showingSlide = (n+testimonialSliders.length) % testimonialSliders.length;
+    testimonialSliders[showingSlide].className = "mySlides activeSlide";
+}
+
+nextTest.addEventListener("click", nextTestimonial);
+prevTest.addEventListener("click", prevTestimonial);
